60caebd008fecfc7093e5926e66e396707278740
Matt Scola <scola.matt@gmail.com>
2020-02-03 20:05:34 -0800

    Implements fully concurrent mandelbrot program

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,5 +1,6 @@
 extern crate num;
 extern crate image;
+extern crate crossbeam;
 
 use std::str::FromStr;
 use num::Complex;
@@ -171,7 +172,28 @@ fn main() {
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render(&mut pixels, bounds, upper_left, bottom_right);
+    // This was for the single-threaded version
+    //render(&mut pixels, bounds, upper_left, bottom_right);
+
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope( |spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, bottom_right);
+                let band_bottom_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, bottom_right);
+
+                spawner.spawn(move || {
+                    render(band, band_bounds, band_upper_left, band_bottom_right);
+                });
+            }
+        });
+    }
 
     write_image(&args[1], &pixels, bounds).expect("Error writing PNG file");
 }
